3d9086d09cd624a9053cbe3c6c0146b0eb995409
Sujan-707 <sujanbharati306@gmail.com>
2026-02-08 12:17:19 +0545

    function added

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React,{ StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import "./index.css"
 
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
      <App />
